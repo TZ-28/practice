9a0cc8366a86c064e2d5d4a4690e5bd97e5d8843
TZ-28 <62976139+TZ-28@users.noreply.github.com>
2020-04-03 00:13:54 -0500

    Update Alpha1.py

--- Alpha1.py
+++ Alpha1.py
@@ -1,8 +1,37 @@
+
 name = input('What is your full name friend?')
 
-team = input('Please pick between the folowing colors. \n 		\33	[1,31,47m Red, Yellow, Pink, or Blue?')
+team = input('Please pick between the folowing colors. \n 		Red, Yellow, Pink, or Blue?')
 
-print("Hello " + name.title()  + ", Welcome to world of 			python programmers. Here many new and old programmers come 	to make what ever their heart desires.")
+month = input('What is the numeric value of the month you were born?')
+day = input('What Day were you born')
+year = input('What year were you born?')
+curYear= input('Please remind me what year is it?')
+curMonth = input('Oh and the month too?')
+age = (int(curYear) - int(year))
+age = int(age)
+if curMonth <= month:
+	age = age
+else: 
+	age = (age - 1)
+
+monconv = {
+	1 : "January",
+	2 : "February",
+	3 : "March",
+	4 : "April",
+	5 : "May",
+	6 : "June",
+  7 : "July",
+	8 : "August",
+	9 : "September",
+	10 : "October",
+  11 : "November",
+	12 : "December",
+
+}
+
+print("Hello " + name.title()  + ", Welcome to world of python programmers. Here many new and old programmers come to make what ever their heart desires.")
 
 if team.title() == 'Blue':
 	print('Welcome to team Sky. We value freedom above all and love to explore.')
@@ -24,4 +53,27 @@ def player(a, b, c):
 	player_1.append(c)
 	return(player_1)
 
+player(name, team, animal)
+
+
 print('Welcome ' + name.title() + ' the ' + team.title() + ' ' + animal.title() + '!')
+
+if day == 1:
+	print("Your were born on the first day of " + monconv.get(int(month)))
+elif day == 2:
+	print("You were born on the Seocnd day of " + monconv.get(int(month)))
+elif day == 3:
+	print("You were born on the third day of " + monconv.get(int(month)))
+else:
+	print("You were born " + (monconv.get(int(month))) + day + "th " + year + '.')
+
+if age >= 50 and age < 100:
+	print(" You are hopefully expiernced or making a career change. ")
+elif age == 23 and age > 16:
+	print('You must be new here. \n Let me know if you have any questions.')
+elif age >= 24 and age < 50:
+	print('Hopefully you like this language more than the one you are switching from. You know my favorite is binary. I I dont really like english.')
+elif age <= 16 and age > 0:
+	print("welp hopefully you understand this more than I do.")
+else:
+	print('goodluck you are ' + str(age) + ' years old.')
