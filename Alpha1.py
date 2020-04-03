2a571660fc5abc035bc22d4a4002fa550098706c
TZ-28 <62976139+TZ-28@users.noreply.github.com>
2020-04-03 00:15:13 -0500

    Update Alpha1.py

--- Alpha1.py
+++ Alpha1.py
@@ -22,11 +22,11 @@ monconv = {
 	4 : "April",
 	5 : "May",
 	6 : "June",
-  7 : "July",
+  	7 : "July",
 	8 : "August",
 	9 : "September",
 	10 : "October",
-  11 : "November",
+  	11 : "November",
 	12 : "December",
 
 }
